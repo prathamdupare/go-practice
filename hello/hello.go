8f1516007089f23e4b8ab10d80032c230fbec345
prathamdupare <prathamdupare@proton.me>
2024-02-27 00:52:10 +0530

    variables

---
+++ hello/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	var intNum int = 32542
+	fmt.Println(intNum)
+
+	var myString string = "Hello"
+
+	fmt.Println(myString)
+
+	var var1, var2 int = 1, 2
+}
